19eaed528b5a8e71e7cadb517eca49d1e0bb4f90
Adrian Szlegel <adrian.szlegel@hotmail.com>
2022-09-07 16:29:21 +0200

    added logic of fetching the data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import Header from './components/Header';
-import CardGrid from './containers/CardGrid';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header";
+import CardGrid from "./containers/CardGrid";
+import { secret } from './secret';
+
+// photos: https://www.pexels.com/api/
+// https://api.pexels.com/v1/photos/:id
+
+const obj = {  
+  method: 'GET',
+  headers: {
+    'Authorization': secret.API_KEY
+  }
+}
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [imagesResponse, setImagesResponse] = useState({});
+  const [gameImages, populateGameImages] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.pexels.com/v1/search?query=animals&per_page=6', obj)
+      .then(response => response.json())
+      .then((data) => {
+        setImagesResponse(data);
+        console.log(data)
+      })
+      .finally(() => {
+        if(imagesResponse) {
+          for(let i = 0; i < 6; ++i) {
+            populateGameImages(gameImages.concat(imagesResponse.photos[i].src.medium))
+          }
+        }
+
+        setLoading(false);
+        console.log('imagesResponse: ', imagesResponse);
+      })
+      .catch((e) => {
+        console.error('Error when fetching the data: ', e);
+      })
+      console.log('final data: ', gameImages);
+  });
+
   return (
     <React.Fragment>
       <Header />
-      <main>
-        <CardGrid />
-      </main>
+      <main>{loading ? "Loading..." : <CardGrid />}</main>
     </React.Fragment>
   );
 }
