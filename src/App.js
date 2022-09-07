1fadee93203702bf2db71183c70d27090790df25
Adrian Szlegel <adrian.szlegel@hotmail.com>
2022-09-07 17:08:44 +0200

    added another api & added select for api & added handler for api select

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { secret } from './secret';
 // photos: https://www.pexels.com/api/
 // https://api.pexels.com/v1/photos/:id
 
+// alternative: https://api.unsplash.com/
+
 const obj = {  
   method: 'GET',
   headers: {
@@ -17,33 +19,48 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [imagesResponse, setImagesResponse] = useState({});
   const [gameImages, populateGameImages] = useState([]);
+  const [apiChosen, setApiChosen] = useState('');
+  const [error, setError] = useState('');
 
-  useEffect(() => {
-    fetch('https://api.pexels.com/v1/search?query=animals&per_page=6', obj)
-      .then(response => response.json())
-      .then((data) => {
-        setImagesResponse(data);
-        console.log(data)
-      })
-      .finally(() => {
-        if(imagesResponse) {
-          for(let i = 0; i < 6; ++i) {
-            populateGameImages(gameImages.concat(imagesResponse.photos[i].src.medium))
+  const PEXELS_URL = 'https://api.pexels.com/v1/search?query=animals&per_page=6';
+  const UNSPLASH_URL = 'https://api.unsplash.com/photos/';
+  
+  const apiHandler = (api) => {
+    console.log('chosen api: ', api)
+    setApiChosen(api);
+  }
+  
+  const fetchAndStart = () => {
+    if(!apiChosen) {
+      setError('Please choose an API to start');
+    } else {
+      fetch(apiChosen === 'pexels' ? PEXELS_URL : UNSPLASH_URL, obj)
+        .then(response => response.json())
+        .then((data) => {
+          setImagesResponse(data);
+          console.log(data)
+        })
+        .finally(() => {
+          if(imagesResponse) {
+            for(let i = 0; i < 6; ++i) {
+              populateGameImages(gameImages.concat(imagesResponse.photos[i].src.medium))
+            }
           }
-        }
 
-        setLoading(false);
-        console.log('imagesResponse: ', imagesResponse);
-      })
-      .catch((e) => {
-        console.error('Error when fetching the data: ', e);
-      })
-      console.log('final data: ', gameImages);
-  });
+          setLoading(false);
+          console.log('imagesResponse: ', imagesResponse);
+        })
+        .catch((e) => {
+          console.error('Error when fetching the data: ', e);
+        })
+        console.log('final data: ', gameImages);
+  }
+  }
 
   return (
     <React.Fragment>
-      <Header />
+      <Header apiHandler={apiHandler} />
+      <button type="button" className="btn btn-primary" onClick={fetchAndStart}>Fetch random photos ans start</button>
       <main>{loading ? "Loading..." : <CardGrid />}</main>
     </React.Fragment>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
-function Header() {
+function Header(props) {
+  const { apiHandler } = props;
+
   return (
     <header className="blog-header py-3">
       <div className="row flex-nowrap justify-content-between align-items-center">
-        <div className="col-12">
+        <div className="col-6">
           <span>React Memory Card Game</span>
           <span>[to be added zoom] + | -</span>
           <span><br/>[to be added select number of cards]</span>
           <span><br/>[to be added select images category]</span>
+        </div>
+
+        <div className="col-6">
+          <label htmlFor="api-select">Select the API to be used</label>
+          <select 
+            name="api-select" 
+            className="form-select" 
+            aria-label="Select the API to be used" 
+            onChange={(event) => apiHandler(event.target.value)}>
+            <option value="pexels">Pexels</option>
+            <option value="unsplash">Unsplash</option>
+          </select>
         </div>
       </div>
     </header>
